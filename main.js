948654742820ce5182cb9f7a4b7615ced71bfd81
Eshaan Gurjar <94111799+Eshaan12345@users.noreply.github.com>
2023-11-14 02:45:46 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function submit() {
+	ognum = document.getElementById("text").value;
+
+	if (ognum % 2 === 0) {
+        givennum = ognum / 2;
+
+		if (givennum = 1) {
+			document.getElementById("proof").innerHTML = "The number " + ognum + " follows the Collats Conjecture.";
+			document.getElementById("text").value = "";
+		}
+
+    } else {
+        givennum = ognum * 3 + 1;
+
+		if (givennum = 1) {
+			document.getElementById("proof").innerHTML = "The number " + ognum + " follows the Collats Conjecture.";
+			document.getElementById("text").value = "";
+		}
+    }
+}
+
+
